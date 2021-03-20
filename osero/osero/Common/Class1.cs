5b06727388c34176e727cfbcdbf2baea667d782e
TA-000 <ta07ka19@yhoo.co.jp>
2021-03-20 13:24:32 +0900

    盤面表示

---
+++ osero/osero/Common/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace osero.Common
+{
+    public class Stone : PictureBox
+    {
+        StoneColor stoneColor = StoneColor.None;
+        public int Colum
+        {
+            protected set;
+            get;
+        } = 0;
+        public int Row
+        {
+            protected set;
+            get;
+        } = 0;
+
+        public Stone(int colum, int row)
+        {
+            Colum = colum;
+            Row = row;
+
+            Click += Stone_Click;
+        }
+
+        public delegate void StoneClickHandler(int x, int y);
+        public event StoneClickHandler StoneClick;
+        private void Stone_Click(object sender, EventArgs e)
+        {
+            StoneClick?.Invoke(Colum, Row);
+        }
+
+        public StoneColor StoneColor
+        {
+            get { return stoneColor; }
+            set
+            {
+                stoneColor = value;
+
+                if (value == StoneColor.Black)
+                    BackColor = Color.Black;
+                if (value == StoneColor.White)
+                    BackColor = Color.White;
+                if (value == StoneColor.None)
+                    BackColor = Color.Green;
+            }
+        }
+    }
+    public enum StoneColor
+    {
+        None = 0,
+        Black = 1,
+        White = 2,
+    }
+}
--- osero/osero/Form1.cs
+++ osero/osero/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using osero.Common;
 
 namespace osero
 {
     public partial class Form1 : Form
     {
+        Point leftTopPoint = new Point(50, 50);
+        Stone[,] StonePosition = new Stone[8, 8];
+
         public Form1()
         {
             InitializeComponent();
@@ -20,5 +24,53 @@ namespace osero
             //}
         }
 
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+          //  this.pictureBox1.BackColor="brac"   
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CreatePictureBoxes();
+            GameStart();
+        }
+        void CreatePictureBoxes()
+        {
+            for (int row = 0; row < 8; row++)
+            {
+                for (int colum = 0; colum < 8; colum++)
+                {
+                    Stone stone = new Stone(colum, row);
+                    stone.Parent = this;
+                    stone.Size = new Size(70, 70);
+                    stone.BorderStyle = BorderStyle.FixedSingle;
+                    stone.Location = new Point(leftTopPoint.X + colum * 70, leftTopPoint.Y + row * 70);
+                    StonePosition[colum, row] = stone;
+                    //stone.StoneClick += Box_PictureBoxExClick;
+                    stone.BackColor = Color.Green;
+                }
+            }
+        }
+
+        void GameStart()
+        {
+            var stones = StonePosition.Cast<Stone>();
+            foreach (Stone stone in stones)
+                stone.StoneColor = StoneColor.None;
+
+            StonePosition[3, 3].StoneColor = StoneColor.Black;
+            StonePosition[4, 4].StoneColor = StoneColor.Black;
+
+            StonePosition[3, 4].StoneColor = StoneColor.White;
+            StonePosition[4, 3].StoneColor = StoneColor.White;
+
+            //isYour = true;
+            //toolStripStatusLabel1.Text = "あなたの手番です。";
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GameStart();
+        }
     }
 }
