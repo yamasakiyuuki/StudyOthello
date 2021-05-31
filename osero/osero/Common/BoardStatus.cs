7f574c4e4156c60adb0ac05574d9842361153555
TA-000 <ta07ka19@yhoo.co.jp>
2021-05-31 10:58:05 +0900

    譜面のクラス作成

---
+++ osero/osero/Common/BoardStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace osero.Common
+{
+    public class BoardStatus
+    {
+        Stone Stone = null;
+        Stone[] ReturnStone = null;
+    }
+}
